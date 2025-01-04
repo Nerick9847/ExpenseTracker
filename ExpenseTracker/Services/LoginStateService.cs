7e21406ca9169dcfec11d00ffd09d7b2ae009cdb
Nerick <131441228+Nerick9847@users.noreply.github.com>
2025-01-04 18:30:36 +0545

    Add project files.

---
+++ ExpenseTracker/Services/LoginStateService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Services
+{
+    public class LoginState
+    {
+        public bool IsLoggedIn { get; private set; } = false;
+
+        public void Login()
+        {
+            IsLoggedIn = true;
+        }
+
+        public void Logout()
+        {
+            IsLoggedIn = false;
+        }
+    }
+
+}
