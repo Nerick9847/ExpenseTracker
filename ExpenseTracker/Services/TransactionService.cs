c16f85376e11f8df2fb35219811183aae699591f
Nerick <131441228+Nerick9847@users.noreply.github.com>
2025-01-14 18:59:17 +0545

    Login page error still not solved.

---
+++ ExpenseTracker/Services/TransactionService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Services
+{
+    public class TransactionService
+    {
+        private List<Transaction> transactions = new List<Transaction>();
+
+        public List<Transaction> GetTransactions()
+        {
+            return transactions;
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            transactions.Add(transaction);
+        }
+
+        public decimal GetTotalIncome()
+        {
+            return transactions.Where(t => t.Type == "Income").Sum(t => t.Amount);
+        }
+
+        public decimal GetTotalExpense()
+        {
+            return transactions.Where(t => t.Type == "Expense").Sum(t => t.Amount);
+        }
+
+        public decimal GetTotalDebt()
+        {
+            return transactions.Where(t => t.Type == "Debt").Sum(t => t.Amount);
+        }
+    }
+
+    public class Transaction
+    {
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Type { get; set; } // "Income", "Expense", "Debt"
+    }
+}
